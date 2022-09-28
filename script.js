006ed6d317c9fca5812b224758f9463a37a7b1b2
gitdela <scriptingdela@gmail.com>
2022-09-28 22:49:41 +0000

    Add event listeners & HTML buttons

--- script.js
+++ script.js
@@ -16,72 +16,165 @@ function computerPlay() {
 
 
 // remember the variables here are separate arguments that are defined as variables inside this function ()s 
-function singleRound(playerSelection, computerSelection) {
-// this console.log call is interesting. look at it carefully
-    // console.log('Player:', playerSelection, 'Computer:', computerSelection)
-// i could've easily used if else and logical operators easily but i decided to use switch case with if else embedded in it
-// since the computer selection was going to be generated auto, i decided to make the cases rep the player selection
-// not that it matters but just for my understanding
+// function singleRound(playerSelection, computerSelection) {
+// // this console.log call is interesting. look at it carefully
+//     // console.log('Player:', playerSelection, 'Computer:', computerSelection)
+// // i could've easily used if else and logical operators easily but i decided to use switch case with if else embedded in it
+// // since the computer selection was going to be generated auto, i decided to make the cases rep the player selection
+// // not that it matters but just for my understanding
+//     switch(playerSelection) {
+//         case 'rock':
+//             if (computerSelection === 'rock') {
+//                 return 'Draw! You both picked Rock';
+//             } else if (computerSelection === 'paper') {
+//                 computerScore++;
+//                 // console.log(`Player score: ${playerScore} Computer score: ${computerScore}`)
+//                 return 'You lose! Rock gets covered by Paper';
+//             } else if (computerSelection === 'scissors') {
+//                 playerScore++;
+//                 // console.log(`Player score: ${playerScore} Computer score: ${computerScore}`)
+//                 return 'You win! Rock crashes Scissors';
+//             }
+//             break;
+//         case 'paper':
+//             if (computerSelection === 'rock') {
+//                 playerScore++;
+//                 return 'You win! Paper covers Rock';
+//             } else if (computerSelection === 'paper') {
+//                 return 'Draw! You both picked Paper';
+//             } else if (computerSelection === 'scissors') {
+//                 computerScore++;
+//                 return 'You lose! Paper gets cut by Scissors';
+//             }
+//             break;
+//         case 'scissors':
+//             if (computerSelection === 'rock') {
+//                 computerScore++;
+//                 return 'You lose! Scissors gets crashed by Rock';
+//             } else if (computerSelection === 'paper') {
+//                 playerScore++;
+//                 return 'You win! Scissors cuts Paper';
+//             } else if (computerSelection === 'scissors') {
+//                 return 'Draw! You both picked Scissors';
+//             }
+//             break;
+//     }
+// }
+
+
+function playRound(playerSelection, computerSelection) {
+    console.log('Player:', playerSelection, 'Computer:', computerSelection)
     switch(playerSelection) {
         case 'rock':
             if (computerSelection === 'rock') {
-                return 'Draw! You both picked Rock';
+                console.log('Draw! You both picked Rock');
             } else if (computerSelection === 'paper') {
-                computerScore++;
-                // console.log(`Player score: ${playerScore} Computer score: ${computerScore}`)
-                return 'You lose! Rock gets covered by Paper';
+                // computerScore++;
+                console.log('You lose! Rock gets covered by Paper');
             } else if (computerSelection === 'scissors') {
-                playerScore++;
-                // console.log(`Player score: ${playerScore} Computer score: ${computerScore}`)
-                return 'You win! Rock crashes Scissors';
+                // playerScore++;
+                console.log('You win! Rock crashes Scissors');
             }
-            break;
+        break;
         case 'paper':
             if (computerSelection === 'rock') {
-                playerScore++;
-                return 'You win! Paper covers Rock';
+                console.log('You win! Paper covers Rock');
             } else if (computerSelection === 'paper') {
-                return 'Draw! You both picked Paper';
+                // computerScore++;
+                console.log('Draw! You both picked Paper');
             } else if (computerSelection === 'scissors') {
-                computerScore++;
-                return 'You lose! Paper gets cut by Scissors';
+                // playerScore++;
+                console.log('You Lose! Paper gets cut by Scissors');
             }
-            break;
+        break;
         case 'scissors':
             if (computerSelection === 'rock') {
-                computerScore++;
-                return 'You lose! Scissors gets crashed by Rock';
+                console.log('You Lose! Scissors get crushed by Rock');
             } else if (computerSelection === 'paper') {
-                playerScore++;
-                return 'You win! Scissors cuts Paper';
+                // computerScore++;
+                console.log('You Win! Scissors cuts Paper');
             } else if (computerSelection === 'scissors') {
-                return 'Draw! You both picked Scissors';
+                // playerScore++;
+                console.log('Draw! You both picked Scissors');
             }
-            break;
+        break;
     }
 }
 
+const rockBtn = document.querySelector('#rock-btn');
+rockBtn.addEventListener('click', (e) => {
+    playRound('rock', computerPlay());
+});
 
-// creating a loop that will play the multiple times
-function game() {
-// here, i like initializing from 1. that will definitely affect the = and <= sings, take care
-    for(i = 1; i <=5; i++) {
-// i had to put the player selection prompt inside the loop so it gets called all 5 times
-// even the computer play needs to return a random element in a loop of 5. that's why it's 
-// toLowerCase is to format any case inside the prompt to become uniform to what's coded
-        let playerSelectionGs = prompt('Select one of rock, paper, scissors: ', 'Case insensitive but check the spelling').toLowerCase();
-        let computerSelectionGs = computerPlay();
-// and these variables have Gs to show it's not the same ones declared in the singleRound()
-        console.log(singleRound(playerSelectionGs, computerSelectionGs));
-        console.log(`Player score: ${playerScore} Computer score: ${computerScore}`)
-    }
-    if (playerScore > computerScore) {
-        console.log('CONGRATULATIONS! YOU WON THE GAME BY '+ (playerScore - computerScore) +' POINT(S)');
-    } else if (computerScore > playerScore) {
-        console.log('YOU LOST BY '+ (computerScore - playerScore) +' POINT(S). BETTER LUCK NEXT TIME');
-    } else {
-        console.log('Draw! Try again');
-    }
-}
+const paperBtn = document.querySelector('#paper-btn');
+paperBtn.addEventListener('click', (e) => {
+    playRound('paper', computerPlay());
+});
 
-game();
+const scissorsBtn = document.querySelector('#scissors-btn');
+scissorsBtn.addEventListener('click', (e) => {
+    playRound('scissors', computerPlay());
+});
+
+
+
+// function paperClicked() {
+
+//     let computerSelectionGs = computerPlay();
+
+//     if (computerSelectionGs === 'rock') {
+//         console.log('win');
+//     }
+//     else if (computerSelectionGs === 'paper') {
+//         console.log('draw');
+//     }
+//     else if (computerSelectionGs === 'scissors') {
+//         console.log('lose')
+//     }
+
+// }
+
+// const paperBtn = document.querySelector('#paper-btn');
+// paperBtn.addEventListener('click', paperClicked);
+
+
+// function scissorsClicked() {
+
+//     let computerSelectionGs = computerPlay();
+    
+//     if (computerSelectionGs === 'rock') {
+//         console.log('lose');
+//     }
+//     else if (computerSelectionGs === 'paper') {
+//         console.log('win');
+//     }
+//     else if (computerSelectionGs === 'scissors') {
+//         console.log('draw')
+//     }
+
+// }
+
+// const scissorsBtn = document.querySelector('#scissors-btn');
+// scissorsBtn.addEventListener('click', scissorsClicked);
+
+// // creating a loop that will play the multiple times
+// function game() {
+// // here, i like initializing from 1. that will definitely affect the = and <= sings, take care
+//     for(i = 1; i <=5; i++) {
+// // i had to put the player selection prompt inside the loop so it gets called all 5 times
+// // even the computer play needs to return a random element in a loop of 5. that's why it's 
+// // toLowerCase is to format any case inside the prompt to become uniform to what's coded
+//         let playerSelectionGs = prompt('Select one of rock, paper, scissors: ', 'Case insensitive but check the spelling').toLowerCase();
+// // and these variables have Gs to show it's not the same ones declared in the singleRound()
+//         console.log(singleRound(playerSelectionGs, computerSelectionGs));
+//         console.log(`Player score: ${playerScore} Computer score: ${computerScore}`)
+//     }
+//     if (playerScore > computerScore) {
+//         console.log('CONGRATULATIONS! YOU WON THE GAME BY '+ (playerScore - computerScore) +' POINT(S)');
+//     } else if (computerScore > playerScore) {
+//         console.log('YOU LOST BY '+ (computerScore - playerScore) +' POINT(S). BETTER LUCK NEXT TIME');
+//     } else {
+//         console.log('Draw! Try again');
+//     }
+// }
+// game();
